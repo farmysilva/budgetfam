0837848cf10ed402fd5768982638d5c344b03ee5
Farmy Silva <farmysilva@yahoo.com.br>
2023-08-22 21:35:20 -0300

    Commit inicial

---
+++ budgetfam/src/main/java/com/farmy/budgetfam/BudgetfamApplication.java
@@ -0,0 +1,13 @@
+package com.farmy.budgetfam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetfamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetfamApplication.class, args);
+	}
+
+}
---
+++ budgetfam/src/test/java/com/farmy/budgetfam/BudgetfamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmy.budgetfam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetfamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
